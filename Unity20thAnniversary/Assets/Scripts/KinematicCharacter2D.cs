1ce6db60e401f4ac69c64b41be80d1f61f5bb151
Gabriel G. Cunha <gabrielmesmo97@gmail.com>
2025-11-10 10:58:27 -0300

    Start making the game after the jam

---
+++ Unity20thAnniversary/Assets/Scripts/KinematicCharacter2D.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicCharacter2D : MonoBehaviour
+{
+    [SerializeField] private float GravityScale = 1f;
+    [SerializeField] private LayerMask _solidLayerMask;
+    [SerializeField] private BoxCollider2D _hitbox;
+
+    protected Rigidbody2D _rb2D;
+    protected Vector2 _velocity;
+
+    private void Awake()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        _velocity += GravityScale * Physics2D.gravity * Time.deltaTime;
+
+        Vector2 deltaPosition = _velocity * Time.deltaTime;
+        Vector2 movementVector = Vector2.up * deltaPosition.y;
+
+        Move(movementVector);
+    }
+
+    private void Move(Vector2 movementVector)
+    {
+        Vector2 newPosition = _rb2D.position;
+
+        Vector2 checkPositionY = _rb2D.position + new Vector2(0, movementVector.y);
+        if (IsCollidingAt(checkPositionY, _solidLayerMask))
+        {
+            Vector2 signedCheckPositionY = _rb2D.position + new Vector2(0, Mathf.Sign(movementVector.y) * Time.deltaTime);
+            while (!IsCollidingAt(signedCheckPositionY, _solidLayerMask))
+            {
+                newPosition.y += Mathf.Sign(movementVector.y) * Time.deltaTime;
+            }
+
+            movementVector.y = 0;
+        }
+
+        newPosition.y += movementVector.y;
+
+        _rb2D.MovePosition(newPosition);
+    }
+    
+    private bool IsCollidingAt(Vector2 positionToCheck, LayerMask layerMask)
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(positionToCheck, _hitbox.size, 0, layerMask);
+        return colliders.Length > 0;
+    }
+}
---
+++ Unity20thAnniversary/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private static PlayerController _instance;
+    public static PlayerController Instance => _instance;
+
+    private Rigidbody2D _rb2D;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+    
+    private void Move(float moveDirection)
+    {
+        Vector2 movementVector = _rb2D.velocity;
+        movementVector.x = moveDirection;
+
+        _rb2D.velocity = movementVector;
+    }
+}
